a8524c6c01c5b95f2b70f03e482fdbcc7878503c
almeidarodrigo04 <rodrigosilvalmeida10@gmail.com>
2025-12-09 23:19:03 -0300

    mensagem

--- fortuna.py
+++ fortuna.py
@@ -1,21 +1,65 @@
+""" 
+    COMO RODAR:
+
+    uv sync (primeira vez)
+    uv run fortuna.py
+
+    lembrar de ajustar parametros a cada vez
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+RADIUS = 1
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+def get_point(n, plot):
+    if plot:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
 
-# Make data
-theta = np.linspace(0, 2 * np.pi, 100)
-phi = np.linspace(0, np.pi, 100)
-x = 10 * np.outer(np.cos(theta), np.sin(phi))
-y = 10 * np.outer(np.sin(theta), np.sin(phi))
-z = 10 * np.outer(np.ones(np.size(theta)), np.cos(phi))
+    points = []
+    for i in range(n):
+        theta = np.random.rand() * 2 * np.pi
+        # maracutaia geminiana, se vc gerar phi uniformemente n fica uniforme na esfera
+        cos_phi = 2 * np.random.rand() - 1 
+        sin_phi = np.sqrt(1 - cos_phi**2) # sin^2(phi) + cos^2(phi) = 1
 
-# Plot the surface
-ax.plot_surface(x, y, z, alpha=0.4)
+        x = RADIUS * np.cos(theta) * sin_phi
+        y = RADIUS * np.sin(theta) * sin_phi
+        z = RADIUS * cos_phi
 
-# Set an equal aspect ratio
-ax.set_aspect('equal')
+        if plot: ax.scatter(x, y, z)
 
-plt.show()
+        points.append((x, y, z))
+
+    if plot:
+        ax.set_aspect('equal')
+        plt.show()
+        plt.close()
+    
+    return np.array(points)
+
+def check_win(p):
+    # sisteminha de equação bolado
+    P = np.array([p[0], p[1], p[2], p[3]]).T
+    A = np.vstack([P, [1, 1, 1, 1]])
+    b = np.array([0, 0, 0, 1])
+    alpha = np.linalg.solve(A, b)
+    if np.all(alpha >= -1e-9):
+            return 1
+    return 0
+
+# Aqui vc escolhe a quantidade de pontos
+N=int(1e6)
+
+# Lembra de falar se vc quer ou n plotar (só precisa pro primeiro passo)
+points = get_point(N, False)
+
+p_chapeu = 0
+for i in range(N//4):
+    pontos= [points[i*4+j] for j in range(4)]
+    p_chapeu += check_win(pontos)
+
+print(p_chapeu/N)
+
+
